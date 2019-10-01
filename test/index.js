94bae925a9433c7bf9e20d8c038fb581170933eb
Daniel Narey <danielnarey@users.noreply.github.com>
2019-09-30 22:51:16 -0400

    sync

--- test/index.js
+++ test/index.js
@@ -8,20 +8,22 @@ const toFunction = (code) => {
   const out = transform(code, { plugins: [plugin] });
   
   //return wrap(out.code);
-  return out.code;
+  return eval(out.code);
 };
 
 
 test('basic', (t) => {
-  const code = `pug\`
+  const code = `wrap(
+    pug\`
       p#pElem Hello,
         em= name
-  \``;
+    \`
+  )`;
   
   const template = toFunction(code);
   
   t.is(
-    template,//({ name: 'Daniel' }),
-    ''//'<p id="pElem">Hello,<em>Daniel</em></p>',
+    template({ name: 'Daniel' }),
+    '<p id="pElem">Hello,<em>Daniel</em></p>',
   );
 });
